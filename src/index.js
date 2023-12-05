96ad38ddb2d8844af8348957f7d4dfb2aa76fe08
ShawnRSidwell <shawnrsidwell@gmail.com>
2023-12-05 12:36:56 -0800

    fix pizza list

--- src/index.js
+++ src/index.js
@@ -12,16 +12,6 @@ function App() {
   );
 }
 
-function Pizza(props) {
-  return (
-    <div>
-      <img src={props.photoName} alt={props.name} />
-      <h3> {props.name}</h3>
-      <p> {props.ingredients}</p>
-      <p> {props.price}</p>
-    </div>
-  );
-}
 function Header() {
   return (
     <header className="header">
@@ -42,6 +32,19 @@ function Menu() {
         />
       ))}
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 function Footer() {
